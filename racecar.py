a268cd701b395904622fbf0dd29a2cebc98d8c6c
vamshigunji183 <31291923+vamshigunji183@users.noreply.github.com>
2017-12-22 22:15:04 +0530

    moving car on x axis
    moving the main character chaing the X value

--- racecar.py
+++ racecar.py
@@ -1,41 +1,51 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
+import pygame
 
-This is a temporary script file.
-"""
-import  pygame
 
-pygame.init() #intiate the pygame
-h=800
-w=600
-gameDisplay=pygame.display.set_mode((h,w)) #window
+pygame.init()
 
-#naming window
-pygame.display.set_caption('car race')
 
-clock=pygame.time.Clock()
-#check what is clock
+display_width = 800
+display_height = 600
 
-crashed= False
+gameDisplay = pygame.display.set_mode((display_width,display_height))
+pygame.display.set_caption('A bit Racey')
+
+black = (0,0,0)
+white = (255,255,255)
+
+clock = pygame.time.Clock()
+crashed = False
+carImg = pygame.image.load('C:/Users/gvvam/OneDrive/Documents/GitHub/pygame/car_F1.png')
+carImg=pygame.transform.scale(carImg, (128, 72))
+def car(x,y):
+    gameDisplay.blit(carImg, (x,y))
+
+x =  (display_width * 0.45)
+y = (display_height * 0.8)
+
+x_change=0
 
 while not crashed:
-    #here we start our loop
     for event in pygame.event.get():
-        #pygame event
         if event.type == pygame.QUIT:
-            #this is handled by pygame
             crashed = True
-            
-        print(event)
-        #this will print the event in the pygame
-    pygame.display.update() #this will update the window or we can use flip
-    #update vs flip
-    
-    #frames per second
-    clock.tick(32)
+        if event.type==pygame.KEYDOWN:
+            if event.key==pygame.K_LEFT:
+                x_change=-5
+            elif event.key==pygame.K_RIGHT:
+                x_change=5
+        if event.type==pygame.KEYUP:
+            if event.key==pygame.K_LEFT or event.key==pygame.K_RIGHT:
+                x_change=0
+                
+                
+    x+=x_change
+    gameDisplay.fill(white)
+    car(x,y)
+
+        
+    pygame.display.update()
+    clock.tick(60)
+
 pygame.quit()
 quit()
-    
-    
-        
